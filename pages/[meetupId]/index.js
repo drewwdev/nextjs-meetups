90ca15f52df599a29ea76f40f205966950b68d89
Drew <dreww.dev@gmail.com>
2021-12-16 10:02:37 -0500

    Added details to meetup components

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/dc/Skyscrapers_of_Shinjuku_2009_January_%28revised%29.jpg"
+      title="First Meetup"
+      address="123 Main St"
+      description="Our First Meetup"
+    />
+  );
+}
+export default MeetupDetails;
