d63a4777c93a9bbac085f99913293f413c06923d
Drew <dreww.dev@gmail.com>
2021-12-15 19:38:27 -0500

    Updated CSS / Added Navigation bar to home page

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,11 @@
 // domain.com/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {}
+  console.log(addMeetupHandler);
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
