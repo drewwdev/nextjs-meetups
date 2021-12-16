65f147f4f4d4605eba35b04136d2be7260c1f3bb
Drew <dreww.dev@gmail.com>
2021-12-15 19:23:37 -0500

    Initial commit/ Home Page initial

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/dc/Skyscrapers_of_Shinjuku_2009_January_%28revised%29.jpg",
+    address: "123 Main St",
+    description: "This is our first meetup",
+  },
+  {
+    id: "m2",
+    title: "Another Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/dc/Skyscrapers_of_Shinjuku_2009_January_%28revised%29.jpg",
+    address: "456 Main St",
+    description: "This is our first meetup",
+  },
+  {
+    id: "m3",
+    title: "A Third Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/dc/Skyscrapers_of_Shinjuku_2009_January_%28revised%29.jpg",
+    address: "789 Main St",
+    description: "This is our first meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
