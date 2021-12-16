8a4482fc8e9459d1dce427f33ae24788bee88742
Drew <dreww.dev@gmail.com>
2021-12-16 10:41:57 -0500

    Learning the difference between getStaticProps and getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -31,12 +31,26 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
 export async function getStaticProps() {
   // fetch data from API
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
+    revalidate: 10,
   };
 }
 
