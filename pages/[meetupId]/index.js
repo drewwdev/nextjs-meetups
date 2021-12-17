c7e3f4894b1012751cddb243bcb3409a655b39d4
Drew <dreww.dev@gmail.com>
2021-12-17 12:52:51 -0500

    Dynamic Pages / getStaticProps/Paths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,4 +10,44 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/dc/Skyscrapers_of_Shinjuku_2009_January_%28revised%29.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "123 Main St",
+        description: "Our First Meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
